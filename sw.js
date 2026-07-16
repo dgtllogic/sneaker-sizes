2ded517388d03065ddda0d9f1251d4697f1d2d48
dgtllogic <dgtllogicny@gmail.com>
2026-07-15 21:54:14 -0400

    v12: bigger gender labels in fullscreen

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Size Guide service worker
    Strategy: NETWORK-FIRST for HTML (so deployed updates appear immediately
    when online), CACHE-FIRST for other assets (for speed + offline). */
-const CACHE_VERSION = 'size-guide-v11';
+const CACHE_VERSION = 'size-guide-v12';
 const FILES = [
   './',
   './index.html',
